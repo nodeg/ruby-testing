808eb3f1ad1f4442232de117661eae7eb6e37b46
Dominik Gedon <dominik.gedon@suse.com>
2023-09-06 15:53:33 +0200

    Add top level comment to test class
    Signed-off-by: Dominik Gedon <dominik.gedon@suse.com>

--- tdd/tests/hello_test.rb
+++ tdd/tests/hello_test.rb
@@ -3,6 +3,7 @@
 require 'test/unit'
 require_relative '../hello'
 
+# Simple test class for HelloWorld
 class TestClass < Test::Unit::TestCase
   def test_hello_world
     greeting = HelloWorld.new.greet('World')
